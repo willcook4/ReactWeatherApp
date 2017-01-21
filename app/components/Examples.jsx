e6d85b3c0f1ce1b4de96e023e4bb554a07043cc4
Will Cook <nzwillcook@gmail.com>
2017-01-21 18:46:06 +0000

    added text

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var Examples = (props) => {
   return (
-    <h3>Examples Component goes here!</h3>
+    <div>
+      <h3>Examples Component goes here!</h3>
+      <p>Welcome</p>
+    </div>
   )
 }
 
