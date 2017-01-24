c361401f73170d0838b5d79d88ec41cd46dccdd1
Will Cook <nzwillcook@gmail.com>
2017-01-24 11:15:56 +0000

    Styled examples and main

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,24 +1,13 @@
 var React = require('react');
 var Nav = require('Nav');
 
-// var Main = React.createClass({
-//   render: function(){
-//     return (
-//       <div>
-//         <Nav/>
-//         <h2>Main Component</h2>
-//         {this.props.children}
-//       </div>
-//     );
-//   }
-// });
-
 var Main = (props) => {
   return (
     <div>
       <Nav/>
-      <h2>Main Component</h2>
-      {props.children}
+      <div className="row">
+        <div className="medium-6 large-4 columns small-centered">{props.children}</div>
+      </div>
     </div>
   );
 }
