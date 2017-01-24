efbd01fe5e6edd122d2b544d924776aeef22a0e7
Will Cook <nzwillcook@gmail.com>
2017-01-24 11:28:47 +0000

    atyled about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,19 +1,30 @@
 var React = require('react');
 
-//Now Reafactured to below
-// var About = React.createClass({
-//     render: function () {
-//       return (
-//         <h3>About Component</h3>
-//       )
-//     }
-// });
-
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application built on React. I built this with an online course.</p>
+
+      <div>
+        <h6 className="text-center">Technology used in this app:</h6>
+        <ul>
+          <li>
+            <a href="https://facebook.com.github.io/react">React</a>
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open Weather Map</a> I used Open Weather Map to search for weather data by City name.
+          </li>
+          <li>Webpack</li>
+          <li>Babel</li>
+          <li>ES6 JavaScript</li>
+          <li>Heroku</li>
+          <li>git</li>
+          <li>GitHub</li>
+        </ul>
+      </div>
+
+
     </div>
   )
 };
